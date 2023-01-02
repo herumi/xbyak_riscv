bcbecf52914610340387eaceb9a87981ffee8ab5
MITSUNARI Shigeo <herumi@nifty.com>
2023-01-02 10:41:28 +0900

    add test of addi, slti, sltiu, xori, ori, andi

--- test/gen_test.py
+++ test/gen_test.py
@@ -1,11 +1,19 @@
 import sys
 isXbyak = True
 
-def put3r(name):
+def putRRR(name):
   if isXbyak:
     print(f'{name}(x1, x2, x3); dump1();')
   else:
     print(f'{name.strip("_")} x1, x2, x3')
+
+def putRRI(name):
+  if isXbyak:
+    print(f'{name}(x1, x2, 1234); dump1();')
+    print(f'{name}(x1, x2, -1234); dump1();')
+  else:
+    print(f'{name.strip("_")} x1, x2, 1234')
+    print(f'{name.strip("_")} x1, x2, -1234')
 
 def main():
   global isXbyak
@@ -13,7 +21,10 @@ def main():
     isXbyak = False
 
   for op in ['add', 'sub', 'sll', 'slt', 'sltu', 'xor_', 'srl', 'sra', 'or_', 'and_']:
-    put3r(op)
+    putRRR(op)
+
+  for op in ['addi', 'slti', 'sltiu', 'xori', 'ori', 'andi']:
+    putRRI(op)
 
 
 if __name__ == '__main__':
