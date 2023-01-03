44524f04f26915055112397b27b0f34a40d106d4
MITSUNARI Shigeo <herumi@nifty.com>
2023-01-03 09:01:49 +0900

    [test] rename generated.s

--- test/test.sh
+++ test/test.sh
@@ -8,9 +8,9 @@ python3 gen_test.py > generated.cpp
 $CXX $CFLAGS out.cpp
 ./a.out > my.txt
 
-python3 gen_test.py gas > a.s
-$AS -c -o a.o a.s
-$OBJDUMP --no-addresses -d a.o > ok.s
+python3 gen_test.py gas > generated.s
+$AS -c -o generated.o generated.s
+$OBJDUMP --no-addresses -d generated.o > ok.s
 awk '/        / { print $1 }' < ok.s > ok.txt
 
 diff ok.txt my.txt
