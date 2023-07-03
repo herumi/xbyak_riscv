4ce5f5f01835af6662b6a27a9c35bf5910ceff54
MITSUNARI Shigeo <herumi@nifty.com>
2023-07-03 09:18:47 +0900

    change IReg::toSring() to non virtual function

--- xbyak_riscv/xbyak_riscv.hpp
+++ xbyak_riscv/xbyak_riscv.hpp
@@ -365,15 +365,46 @@ protected:
 	Kind kind_;
 public:
 	constexpr IReg(uint32_t idx = 0, Kind kind = GPR)
-		: idx_(idx), kind_(kind) { }
+		: idx_(idx), kind_(kind)
+	{
+		XBYAK_RISCV_ASSERT(local::inBit(idx, 5));
+	}
 	constexpr int getIdx() const { return idx_; }
+	const char *toString() const
+	{
+		if (kind_ == GPR) {
+			static const char tbl[][4] = {
+				"x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7",
+				"x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15",
+				"x16", "x17", "x18", "x19", "x20", "x21", "x22", "x23",
+				"x24", "x25", "x26", "x27", "x28", "x29", "x30", "x31",
+			};
+			return tbl[idx_];
+		} else if (kind_ == FReg) {
+			static const char tbl[][4] = {
+				"f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7",
+				"f8", "f9", "f10", "f11", "f12", "f13", "f14", "f15",
+				"f16", "f17", "f18", "f19", "f20", "f21", "f22", "f23",
+				"f24", "f25", "f26", "f27", "f28", "f29", "f30", "f31",
+			};
+			return tbl[idx_];
+		} else if (kind_ == VECTOR) {
+			static const char tbl[][4] = {
+				"v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7",
+				"v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15",
+				"v16", "v17", "v18", "v19", "v20", "v21", "v22", "v23",
+				"v24", "v25", "v26", "v27", "v28", "v29", "v30", "v31",
+			};
+			return tbl[idx_];
+		}
+		XBYAK_RISCV_THROW_RET(ERR_INTERNAL, 0);
+	}
 	bool operator==(const IReg& rhs) const
 	{
 		return idx_ == rhs.idx_ && kind_ == rhs.kind_;
 	}
 	bool operator!=(const IReg& rhs) const { return !operator==(rhs); }
 
-	virtual const char *toString() const = 0;
 };
 
 } // local
@@ -381,15 +412,6 @@ public:
 // General Purpose Register
 struct Reg : public local::IReg {
 	explicit constexpr Reg(int idx = 0) : local::IReg(idx, IReg::Kind::GPR) { }
-	const char *toString() const override {
-		static const char tbl[][4] = {
-				"x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7",
-				"x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15",
-				"x16", "x17", "x18", "x19", "x20", "x21", "x22", "x23",
-				"x24", "x25", "x26", "x27", "x28", "x29", "x30", "x31",
-        };
-		return tbl[idx_];
-    }
 };
 
 static constexpr Reg x0(0), x1(1), x2(2), x3(3), x4(4), x5(5), x6(6), x7(7);
@@ -416,15 +438,6 @@ static constexpr Reg t3(x28), t4(x29), t5(x30), t6(x31);
 // Floating Point Register
 struct FReg : public local::IReg {
 	explicit constexpr FReg(int idx = 0) : local::IReg(idx, IReg::Kind::FReg) { }
-	const char *toString() const override {
-		static const char tbl[][4] = {
-            "f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7",
-            "f8", "f9", "f10", "f11", "f12", "f13", "f14", "f15",
-            "f16", "f17", "f18", "f19", "f20", "f21", "f22", "f23",
-            "f24", "f25", "f26", "f27", "f28", "f29", "f30", "f31",
-		};
-		return tbl[idx_];
-    }
 };
 
 static constexpr FReg f0(0), f1(1), f2(2), f3(3), f4(4), f5(5), f6(6), f7(7);
@@ -441,15 +454,6 @@ static constexpr FReg ft8(28), ft9(29), ft10(30), ft11(31);
 // Vector Register
 struct VReg : public local::IReg {
 	explicit constexpr VReg(int idx = 0) : local::IReg(idx, IReg::Kind::VECTOR) { }
-    const char *toString() const override {
-		static const char tbl[][4] = {
-				"v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7",
-				"v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15",
-				"v16", "v17", "v18", "v19", "v20", "v21", "v22", "v23",
-				"v24", "v25", "v26", "v27", "v28", "v29", "v30", "v31",
-		};
-		return tbl[idx_];
-    }
 };
 
 static constexpr VReg v0(0), v1(1), v2(2), v3(3), v4(4), v5(5), v6(6), v7(7);
